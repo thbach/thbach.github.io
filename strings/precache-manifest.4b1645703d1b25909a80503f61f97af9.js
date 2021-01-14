6fdd8a4bcd8db07db365ebe9cf19bb847504c3eb
Thai Bach <thbach@gmail.com>
2021-01-13 23:29:48 -0800

    strings

---
+++ strings/precache-manifest.4b1645703d1b25909a80503f61f97af9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb934c570e4bbf05d151b3b9ee21d76f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20801f15cb6ef6f40933",
+    "url": "/static/css/2.032730b7.chunk.css"
+  },
+  {
+    "revision": "20801f15cb6ef6f40933",
+    "url": "/static/js/2.6dd73359.chunk.js"
+  },
+  {
+    "revision": "4d92265ee4745234cbf941e19ca6c02e",
+    "url": "/static/js/2.6dd73359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ebf3bbffa03d950ec58",
+    "url": "/static/js/main.3d9d8a6f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
